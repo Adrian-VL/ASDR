f19007489e9d72f380f450f86ec17c4b2a0b1530
Adrian-VL <129983239+Adrian-VL@users.noreply.github.com>
2023-12-28 15:02:49 -0600

    Update ASDR.java

--- ASDR.java
+++ ASDR.java
@@ -1,1 +1,19 @@
 
+public class Interprete {
+
+  static boolean existenErrores = false;
+
+  public static void main(String[] args) throws IOException {
+        if(args.length > 1) {
+            System.out.println("Uso correcto: interprete [archivo.txt]");
+
+            // Convención defininida en el archivo "system.h" de UNIX
+            System.exit(64);
+        } else if(args.length == 1){
+            ejecutarArchivo(args[0]);
+        } else{
+            ejecutarPrompt();
+        }
+    }
+
+}
