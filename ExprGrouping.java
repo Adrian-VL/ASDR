434d871a7470f3148689146b678caccb9d797946
LuisMarin321 <104316339+LuisMarin321@users.noreply.github.com>
2024-01-14 13:09:28 -0600

    Update ExprGrouping.java

--- ExprGrouping.java
+++ ExprGrouping.java
@@ -1,9 +1,25 @@
-package mx.ipn.escom.k.parser;
-
+// Define la clase ExprGrouping que extiende de Expression. Esta clase se usa para representar una expresión entre paréntesis.
 public class ExprGrouping extends Expression {
+    // Variable para almacenar la expresión interna del agrupamiento.
     final Expression expression;
 
+    // Constructor de ExprGrouping. Toma una expresión como argumento y la almacena.
     ExprGrouping(Expression expression) {
-        this.expression = expression;
+        this.expression = expression; // Asigna la expresión pasada al constructor a la variable de la clase.
+    }
+
+    // Sobrescribe el método toString para convertir la expresión de agrupamiento en una cadena de texto.
+    @Override
+    public String toString() {
+        // Devuelve la representación en cadena de la expresión, encerrada entre paréntesis.
+        return "(" + expression + ")";
+    }
+
+    // Sobrescribe el método solve para resolver la expresión de agrupamiento.
+    @Override
+    Object solve(TablaSimbolos tabla) {
+        // Llama al método solve de la expresión contenida y devuelve su resultado.
+        // La tabla de símbolos es pasada como argumento para permitir la resolución de la expresión.
+        return expression.solve(tabla);
     }
 }
