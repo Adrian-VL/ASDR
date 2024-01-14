e44a669d18ad07ef6186ef29db77567efbe0e6e6
Adrian-VL <129983239+Adrian-VL@users.noreply.github.com>
2024-01-13 19:52:42 -0600

    Update ExprUnary.java

--- ExprUnary.java
+++ ExprUnary.java
@@ -1,7 +1,3 @@
-package mx.ipn.escom.k.parser;
-
-import mx.ipn.escom.k.tools.Token;
-
 public class ExprUnary extends Expression{
     final Token operator;
     final Expression right;
@@ -9,5 +5,40 @@ public class ExprUnary extends Expression{
     ExprUnary(Token operator, Expression right) {
         this.operator = operator;
         this.right = right;
+    }
+    /* Agregado */
+    /*Este fragmento de código define una clase llamada ExprUnary que extiende la clase Expression. 
+    Esta clase representa expresiones unarias, es decir, expresiones que involucran un único operando y
+    un operador unario, como el operador de negación (!) o el operador de negativo numérico (-).*/
+    @Override
+    public String toString(){
+        return operator.lexema + right;
+    }
+
+    @Override
+    Object solve(TablaSimbolos tabla){
+        try{
+            switch (operator.tipo) {
+                case BANG:
+                    if(right.solve(tabla) instaneof Boolean)
+                        return !((boolean) right.solve(tabla));
+                    else
+                        throw new RuntimeException(message: "Error semantico: Operando no compatible");
+                case MINUS:
+                    if(right.solve(tabla) instanceof Integer)
+                        return -((Integer) right.solve(tabla));
+                    else if(right.solve(tabla) instanceof Float)
+                        return -((Float) right.solve(tabla));
+                    else if(right.solve(tabla) instanceof Double)
+                        return -((Double) right.solve(tabla));
+                    else 
+                        throw new RuntimeException("Error semantico: Operando no compatible");
+                default:
+                    break;
+            }
+        } catch (Excerption e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
