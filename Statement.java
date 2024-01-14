d804db8d1c26b008d78275d8259ead9fe3f8684f
Adrian-VL <129983239+Adrian-VL@users.noreply.github.com>
2024-01-13 20:01:35 -0600

    Update Statement.java

--- Statement.java
+++ Statement.java
@@ -1,4 +1,6 @@
-package mx.ipn.escom.k.parser;
-
+/*Esta clase abstracta Statement sirve como base para otras clases que representarán diferentes tipos de declaraciones en un lenguaje de programación o sistema de interpretación.
+  Las clases que herede de esta deben proporcionar una implementación concreta para el método solve, 
+  definiendo cómo se deben ejecutar las instrucciones específicas en el contexto de una tabla de símbolos.*/
 public abstract class Statement {
+  abstract void solve(TablaSimbolos tabla);
 }
