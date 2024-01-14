358fb39dd90d2593086d1dbf56a2af69c8313465
Adrian-VL <129983239+Adrian-VL@users.noreply.github.com>
2024-01-13 19:59:38 -0600

    Update ExprVariable.java

--- ExprVariable.java
+++ ExprVariable.java
@@ -1,11 +1,23 @@
-package mx.ipn.escom.k.parser;
-
-import mx.ipn.escom.k.tools.Token;
-
 class ExprVariable extends Expression {
     final Token name;
 
     ExprVariable(Token name) {
         this.name = name;
+    }
+//Agregado
+/* sta clase representa una expresión que simplemente consiste en una variable y proporciona métodos para obtener su representación como cadena (toString())
+    y para resolver su valor en el contexto de una tabla de símbolos (solve).
+    Además, realiza una verificación semántica para asegurarse de que la variable esté declarada antes de intentar obtener su valor. */
+    @Override
+    public String toString(){
+        return name.lexema;
+    }
+
+    @Override
+    object solve(TablaSimbolos tabla){
+        if(!tabla.existeIdentificador(name.lexema))
+            throw new RuntimeException("La variable" + name.lexema + "n ha sido declarada");
+        else
+            return tabla.obtener(name.lexema);
     }
 }
