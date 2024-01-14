5dd806b7ef30abba9d16a4f5de2f4805d11a3d56
Adrian-VL <129983239+Adrian-VL@users.noreply.github.com>
2024-01-13 20:14:34 -0600

    Update StmtExpression.java

--- StmtExpression.java
+++ StmtExpression.java
@@ -1,9 +1,17 @@
-package mx.ipn.escom.k.parser;
-
 public class StmtExpression extends Statement {
     final Expression expression;
 
     StmtExpression(Expression expression) {
         this.expression = expression;
+    }
+    //Agregado
+    //Esta clase representa una declaración de expresión, donde la expresión se evalúa en el contexto de la tabla de símbolos.
+    @Override
+    public String toString(){
+        return expression.toString();
+    }
+    @Override
+    void solve(TablaSimbolos tabla){
+        expression.solve(tabla);
     }
 }
