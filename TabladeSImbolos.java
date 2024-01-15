38dffd519bca93330d6f265b4f334690cedb7eb7
Adrian-VL <129983239+Adrian-VL@users.noreply.github.com>
2024-01-14 21:42:29 -0600

    Update TabladeSImbolos.java

--- TabladeSImbolos.java
+++ TabladeSImbolos.java
@@ -1,1 +1,60 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TablaSimbolos {
+
+    private final Map<String, Object> values = new HashMap<>();
+    final TablaSimbolos anterior;
+
+    TablaSimbolos(TablaSimbolos anterior){
+        this.anterior = anterior;
+    }
+
+    boolean existeIdentificador(String identificador){
+        if(!values.containsKey(identificador)){
+            return buscarEnAnteriores(identificador, this.anterior);
+        }
+        else
+            return true;
+    }
+
+    Object obtener(String identificador) {
+        if (values.containsKey(identificador)) {
+            return values.get(identificador);
+        }
+        return obtenerDeAnteriores(identificador, this.anterior);
+    }
+
+
+    void asignar(String identificador, Object valor){
+        values.put(identificador, valor);
+    }
+
+    private boolean buscarEnAnteriores(String identificador, TablaSimbolos tablaAnterior){
+        if(tablaAnterior == null) 
+            return false;
+        if(!tablaAnterior.values.containsKey(identificador))
+            return buscarEnAnteriores(identificador, tablaAnterior.anterior);
+        else
+            return true;
+    }
+
+    private Object obtenerDeAnteriores(String identificador, TablaSimbolos tablaAnterior){
+        if(tablaAnterior == null)
+            throw new RuntimeException("Variable no definida '" + identificador + "'.");
+        if(!tablaAnterior.values.containsKey(identificador))
+            return obtenerDeAnteriores(identificador, tablaAnterior.anterior);
+        else
+            return tablaAnterior.values.get(identificador);
+    }
+
+    public TablaSimbolos obtenerTabla(String identificador, TablaSimbolos tablaAnterior){
+        if(tablaAnterior == null)
+            throw new RuntimeException("Variable no definida '" + identificador + "'.");
+        if(!tablaAnterior.values.containsKey(identificador))
+            return obtenerTabla(identificador, tablaAnterior.anterior);
+        else
+            return tablaAnterior;
+    }
+}
 
