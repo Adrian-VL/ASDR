d16023214ed9bdb32a835e639db06bf821af878b
Adrian-VL <129983239+Adrian-VL@users.noreply.github.com>
2024-01-15 08:56:54 -0600

    Update Interprete.java

--- Interprete.java
+++ Interprete.java
@@ -56,7 +56,7 @@ public class Interprete {
             List<Statement> dclrtns = parser.parse();//Utiliza el parser para analizar la lista de tokens y generar una lista de declaraciones (Statement). La lista resultante se almacena en la variable dclrtns.
             TablaSimbolos tabla = new TablaSimbolos(null);//Crea una instancia de la clase TablaSimbolos llamada tabla. La inicializa con un contexto padre null. Esto parece indicar que esta será la tabla de símbolos principal.
             for (Statement statement : dclrtns){// Itera sobre cada declaración en la lista dclrtns y resuelve cada declaración en el contexto de la tabla de símbolos tabla utilizando el método solve.
-                statement.solve(tabla);
+                statement.solve(tablaSimbolos);
             }
             //****Agregado****
         }
