014bf74e1a6122ec7807e1d619058f6661bcbbbb
Albertovdc <114888921+Albertovdc@users.noreply.github.com>
2024-01-14 22:08:21 -0600

    Update StmtReturn.java

--- StmtReturn.java
+++ StmtReturn.java
@@ -1,9 +1,23 @@
-package mx.ipn.escom.k.parser;
-
 public class StmtReturn extends Statement {
     final Expression value;
 
     StmtReturn(Expression value) {
         this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "return " + value +";\n";
+    }
+
+    @Override
+    void solve(TablaSimbolos tabla){
+        TablaSimbolos tablaReturn;
+        if(tabla.existeIdentificador("return")){
+            tablaReturn = tabla.obtenerTabla("return", tabla);
+            tablaReturn.asignar("return", value.solve(tablaReturn));
+        }
+        else
+            throw new RuntimeException("Error semantico: uso incorrecto de return");
     }
 }
