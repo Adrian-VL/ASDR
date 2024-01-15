cf508840869df3f338e2cfbe2ec526c6133bf254
Albertovdc <114888921+Albertovdc@users.noreply.github.com>
2024-01-14 22:10:29 -0600

    Update StmtPrint.java

--- StmtPrint.java
+++ StmtPrint.java
@@ -1,9 +1,16 @@
-package mx.ipn.escom.k.parser;
-
 public class StmtPrint extends Statement {
     final Expression expression;
 
     StmtPrint(Expression expression) {
         this.expression = expression;
+    }
+
+    @Override
+    public String toString() {
+        return "print " + expression + ";\n";
+    }
+    @Override
+    void solve(TablaSimbolos tabla) {
+        System.out.println(expression.solve(tabla));
     }
 }
