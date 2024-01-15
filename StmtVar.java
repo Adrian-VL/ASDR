3dadebc4b77d4b1df29c40353934bcf96519d75a
Albertovdc <114888921+Albertovdc@users.noreply.github.com>
2024-01-14 22:04:02 -0600

    Update StmtVar.java

--- StmtVar.java
+++ StmtVar.java
@@ -1,13 +1,25 @@
-package mx.ipn.escom.k.parser;
-
-import mx.ipn.escom.k.tools.Token;
-
 public class StmtVar extends Statement {
-    final Token name;
+    Token name;
     final Expression initializer;
 
     StmtVar(Token name, Expression initializer) {
         this.name = name;
         this.initializer = initializer;
+    }
+    @Override
+    public String toString() {
+        return "var " + name.lexema + " = " + initializer + ";\n";
+    }
+
+    @Override
+    void solve(TablaSimbolos tabla){
+        if (initializer.solve(tabla) instanceof Integer || initializer.solve(tabla) instanceof Float || initializer.solve(tabla) instanceof Double || initializer.solve(tabla) instanceof String || initializer.solve(tabla) instanceof Boolean){
+            if(tabla.existeIdentificador(name.lexema))
+                throw new RuntimeException("Error: " + name.lexema + "se declaró previamente.");
+            Object value = initializer.solve(tabla);
+            tabla.asignar(name.lexema, value);
+        }
+        else
+            throw new RuntimeException("Error semantico: Asignación incompatible");
     }
 }
